4218f2b117055386caa9b72ffe74ffcc6495c325
Vitaliy <theyamshikov@yandex.ru>
2018-04-08 01:16:14 +0600

    refacto

--- src/console/controllers/OpenServerController.php
+++ src/console/controllers/OpenServerController.php
@@ -3,20 +3,33 @@
 namespace yii2module\tool\console\controllers;
 
 use Yii;
+use yii\helpers\ArrayHelper;
+use yii2lab\console\helpers\Output;
 use yii2lab\console\yii\console\Controller;
 
 /**
- * Grabber tools
+ * Open server tools
  */
 class OpenServerController extends Controller
 {
 	
 	/**
-	 * Grab
+	 * Update domains config
 	 */
-	public function actionRun()
+	public function actionUpdate()
 	{
-		Yii::$domain->tool->openServer->run();
+		$collection = Yii::$domain->tool->openServer->update();
+		$hosts = ArrayHelper::getColumn($collection, 'host');
+		Output::items($hosts, count($collection) . ' hosts');
+		Output::line();
+	}
+	
+	public function actionAll()
+	{
+		$collection = Yii::$domain->tool->openServer->all();
+		$hosts = ArrayHelper::getColumn($collection, 'host');
+		Output::items($hosts, count($collection) . ' hosts');
+		Output::line();
 	}
 	
 }
--- src/domain/services/OpenServerService.php
+++ src/domain/services/OpenServerService.php
@@ -2,21 +2,26 @@
 
 namespace yii2module\tool\domain\services;
 
-use yii2lab\console\helpers\Output;
 use yii2lab\domain\services\BaseService;
 
 /**
  * Class OpenServerService
  *
  * @package yii2module\tool\domain\services
+ *
  * @property \yii2module\tool\domain\repositories\file\OpenServerRepository $repository
  */
 class OpenServerService extends BaseService {
 	
-	public function run() {
+	public function update() {
+		$collection = $this->repository->scan();
+		$this->repository->saveToConfig($collection);
+		return $collection;
+	}
+	
+	public function all() {
 		$collection = $this->repository->all();
-		$this->repository->save($collection);
-		Output::block('total: ' . count($collection) . ' domains', 'success!!!');
+		return $collection;
 	}
 	
 }
