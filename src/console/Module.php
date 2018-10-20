58ba6c534ebad542cd19c8d82ae001a639b878b8
Vitaliy <theyamshikov@yandex.ru>
2018-10-20 23:52:25 +0600

    make autoload domain

--- src/console/Module.php
+++ src/console/Module.php
@@ -2,6 +2,8 @@
 
 namespace yii2module\tool\console;
 
+use yii2lab\domain\helpers\DomainHelper;
+
 /**
  * Class Module
  * 
@@ -10,5 +12,12 @@ namespace yii2module\tool\console;
 class Module extends \yii\base\Module {
 
 	//public static $langDir = 'yii2module/tool/domain/messages';
-
+	
+	public function init() {
+		DomainHelper::forgeDomains([
+			'tool' => 'yii2module\tool\domain\Domain',
+		]);
+		parent::init();
+	}
+	
 }
